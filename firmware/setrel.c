c677868b12f20d6a84f7cfc71b64a0ad7f1d8408
Matei Conovici <mconovici@gmail.com>
2013-07-15 16:15:13 +0300

    Modified setrel.c on the pi

--- firmware/setrel.c
+++ firmware/setrel.c
@@ -11,13 +11,32 @@
 #include <fcntl.h>
 
 static char *device = "/dev/i2c-1";
-static uint8_t val = 0xff;
 
-static uint8_t buffer[256];
+#define REGS_LEN 144
+static uint8_t buffer[REGS_LEN];
+
+static void print_buffer(uint8_t *buffer, size_t len)
+{
+	int i;
+
+	for (i = 0; i < len; i++) {
+		if ((i % 16 == 0) && i)
+			printf("\n");
+
+		if ((i % 16 == 0))
+			printf("%04x: ", i);
+
+		printf("%02x ", buffer[i]);
+	}
+
+	printf("\n");
+}
 
 int main(int argc, char **argv)
 {
 	int i, fd;
+	uint8_t val = 0;
+	int tx = 0;
 
 	while ((i = getopt(argc, argv, "d:")) != -1) {
 		switch (i) {
@@ -27,8 +46,10 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (argc > 1)
+	if (argc > 1) {
+		tx = 1;
 		val = atoi(argv[1]);
+	}
 
 	if ((fd = open(device, O_RDWR)) < 0) {
 		perror("open");
@@ -40,9 +61,16 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	buffer[0] = 16;
-	buffer[1] = val;
-	write(fd, buffer, 2);
+	if (tx) {
+		buffer[0] = 1;
+		buffer[1] = val;
+
+		write(fd, buffer, 2);
+	}
+
+	printf("read: %d\n", read(fd, buffer, REGS_LEN));
+
+	print_buffer(buffer, REGS_LEN);
 
 	close(fd);
 
